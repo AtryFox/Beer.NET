702aa8ec46a879c1e61f484b60cb0f4126ec01c5
Benjamin Leuckefeld <code@deratrox.de>
2016-05-14 20:55:35 +0200

    added documentation

--- Beer.NET/Beer.cs
+++ Beer.NET/Beer.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections.Generic;
-using System.Text;
 
 namespace DerAtrox.BeerNET {
 	public class Beer {
+		/// <summary>
+		/// All letters of the latin alphabet which are used in Beer.
+		/// </summary>
 		private static readonly List<char> LowerCase = new List<char>() { 'q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'y', 'x', 'c', 'v', 'b', 'n', 'm' };
 
+		/// <summary>
+		/// Serializes a string into Beer.
+		/// </summary>
+		/// <param name="input">The string to serialize.</param>
+		/// <returns>Serialized string.</returns>
 		public static string SerializeBeer(string input) {
 			input = input.Replace(".", "BEER-BEER∫");
 			input = input.Replace(",", "BEER_BEER∫");
@@ -21,6 +28,11 @@ namespace DerAtrox.BeerNET {
 			return input;
 		}
 
+		/// <summary>
+		/// Deserializes a string from Beer.
+		/// </summary>
+		/// <param name="input">The string to deserialize.</param>
+		/// <returns>Deserialized string.</returns>
 		public static string DeserializeBeer(string input) {
 			input = input.Replace("BEER-BEER∫", ".");
 			input = input.Replace("BEER_BEER∫", ",");
@@ -31,12 +43,18 @@ namespace DerAtrox.BeerNET {
 				}
 
 				input = input.Replace(RepeateString('∫', "BEER", i), LowerCase[i].ToString());
-
 			}
 
 			return input;
 		}
 
+		/// <summary>
+		/// Creates a repated string and adds a final character.
+		/// </summary>
+		/// <param name="finalChar">The last character to add to the string.</param>
+		/// <param name="stringToRepeat">The string to repeat.</param>
+		/// <param name="count">The number of times the stringToRepeat is repeated.</param>
+		/// <returns>Repeated string with final character.</returns>
 		private static string RepeateString(char finalChar, string stringToRepeat, int count) {
 			string result = stringToRepeat;
 
