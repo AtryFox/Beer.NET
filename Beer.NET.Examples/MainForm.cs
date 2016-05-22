f1d837cb67e01b1893d65265c8ca139e10a3bf6c
Benjamin Leuckefeld <code@deratrox.de>
2016-05-22 16:38:00 +0200

    now using spaces instead of tabs

--- Beer.NET.Examples/MainForm.cs
+++ Beer.NET.Examples/MainForm.cs
@@ -2,21 +2,21 @@
 using System.Windows.Forms;
 
 namespace DerAtrox.BeerNET.Examples {
-	public partial class MainForm : Form {
-		public MainForm() {
-			InitializeComponent();
-		}
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
 
-		private void btnGo_Click(object sender, EventArgs e) {
-			txtOutput.Clear();
+        private void btnGo_Click(object sender, EventArgs e) {
+            txtOutput.Clear();
 
-			if (optSBeer.Checked) {
-				txtOutput.Text = Beer.SerializeBeer(txtInput.Text);
-			} else if (optDBeer.Checked) {
-				txtOutput.Text = Beer.DeserializeBeer(txtInput.Text);
-			} else {
-				txtOutput.Text = Beer.DeserializeBeer(Beer.SerializeBeer(txtInput.Text));
-			}
-		}
-	}
+            if (optSBeer.Checked) {
+                txtOutput.Text = Beer.SerializeBeer(txtInput.Text);
+            } else if (optDBeer.Checked) {
+                txtOutput.Text = Beer.DeserializeBeer(txtInput.Text);
+            } else {
+                txtOutput.Text = Beer.DeserializeBeer(Beer.SerializeBeer(txtInput.Text));
+            }
+        }
+    }
 }
