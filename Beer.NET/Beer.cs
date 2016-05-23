82544397c5815042cbab4efd633e0a20a3b1bd83
Henrik Ilgen <Henrik.Ilgen@ActiveVB.de>
2016-05-23 12:21:45 +0200

    Fixed more whitespace :D

--- Beer.NET/Beer.cs
+++ Beer.NET/Beer.cs
@@ -1,68 +1,68 @@
 ﻿using System.Collections.Generic;
 
 namespace DerAtrox.BeerNET {
-	public class Beer : IBeerEncoder {
-		/// <summary>
-		/// All letters of the latin alphabet which are used in Beer.
-		/// </summary>
-		private static readonly List<char> LowerCase = new List<char>() { 'q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'y', 'x', 'c', 'v', 'b', 'n', 'm' };
+    public class Beer : IBeerEncoder {
+        /// <summary>
+        /// All letters of the latin alphabet which are used in Beer.
+        /// </summary>
+        private static readonly List<char> LowerCase = new List<char>() { 'q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'y', 'x', 'c', 'v', 'b', 'n', 'm' };
 
-		/// <summary>
-		/// Serializes a string into Beer.
-		/// </summary>
-		/// <param name="input">The string to serialize.</param>
-		/// <returns>Serialized string.</returns>
-		public string Encode(string input) {
-			input = input.Replace(".", "BEER-BEER∫");
-			input = input.Replace(",", "BEER_BEER∫");
+        /// <summary>
+        /// Serializes a string into Beer.
+        /// </summary>
+        /// <param name="input">The string to serialize.</param>
+        /// <returns>Serialized string.</returns>
+        public string Encode(string input) {
+            input = input.Replace(".", "BEER-BEER∫");
+            input = input.Replace(",", "BEER_BEER∫");
 
-			for (int i = 0; i < LowerCase.Count; i++) {
-				if (!new List<char>() { 'b', 'e', 'r' }.Contains(LowerCase[i])) {
-					input = input.Replace(LowerCase[i].ToString().ToUpper(), RepeateString('∫', "µ", i));
-				}
+            for (int i = 0; i < LowerCase.Count; i++) {
+                if (!new List<char>() { 'b', 'e', 'r' }.Contains(LowerCase[i])) {
+                    input = input.Replace(LowerCase[i].ToString().ToUpper(), RepeateString('∫', "µ", i));
+                }
 
-				input = input.Replace(LowerCase[i].ToString(), RepeateString('∫', "BEER", i));
+                input = input.Replace(LowerCase[i].ToString(), RepeateString('∫', "BEER", i));
 
-			}
+            }
 
-			return input;
-		}
+            return input;
+        }
 
-		/// <summary>
-		/// Deserializes a string from Beer.
-		/// </summary>
-		/// <param name="input">The string to deserialize.</param>
-		/// <returns>Deserialized string.</returns>
-		public string Decode(string input) {
-			input = input.Replace("BEER-BEER∫", ".");
-			input = input.Replace("BEER_BEER∫", ",");
+        /// <summary>
+        /// Deserializes a string from Beer.
+        /// </summary>
+        /// <param name="input">The string to deserialize.</param>
+        /// <returns>Deserialized string.</returns>
+        public string Decode(string input) {
+            input = input.Replace("BEER-BEER∫", ".");
+            input = input.Replace("BEER_BEER∫", ",");
 
-			for (int i = LowerCase.Count - 1; i >= 0; i--) {
-				if (!new List<char>() { 'b', 'e', 'r' }.Contains(LowerCase[i])) {
-					input = input.Replace(RepeateString('∫', "µ", i), LowerCase[i].ToString().ToUpper());
-				}
+            for (int i = LowerCase.Count - 1; i >= 0; i--) {
+                if (!new List<char>() { 'b', 'e', 'r' }.Contains(LowerCase[i])) {
+                    input = input.Replace(RepeateString('∫', "µ", i), LowerCase[i].ToString().ToUpper());
+                }
 
-				input = input.Replace(RepeateString('∫', "BEER", i), LowerCase[i].ToString());
-			}
+                input = input.Replace(RepeateString('∫', "BEER", i), LowerCase[i].ToString());
+            }
 
-			return input;
-		}
+            return input;
+        }
 
-		/// <summary>
-		/// Creates a repated string and adds a final character.
-		/// </summary>
-		/// <param name="finalChar">The last character to add to the string.</param>
-		/// <param name="stringToRepeat">The string to repeat.</param>
-		/// <param name="count">The number of times the stringToRepeat is repeated.</param>
-		/// <returns>Repeated string with final character.</returns>
-		private static string RepeateString(char finalChar, string stringToRepeat, int count) {
-			string result = stringToRepeat;
+        /// <summary>
+        /// Creates a repated string and adds a final character.
+        /// </summary>
+        /// <param name="finalChar">The last character to add to the string.</param>
+        /// <param name="stringToRepeat">The string to repeat.</param>
+        /// <param name="count">The number of times the stringToRepeat is repeated.</param>
+        /// <returns>Repeated string with final character.</returns>
+        private static string RepeateString(char finalChar, string stringToRepeat, int count) {
+            string result = stringToRepeat;
 
-			for (int i = 0; i < count; i++) {
-				result += stringToRepeat;
-			}
+            for (int i = 0; i < count; i++) {
+                result += stringToRepeat;
+            }
 
-			return result + finalChar;
-		}
-	}
+            return result + finalChar;
+        }
+    }
 }
